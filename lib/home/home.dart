59472333a8fbb9eb5e0cfa48e0b2d96794dcf910
Ivan Trajanovski <ivan.trajanovski@egroup.hu>
2022-02-10 15:10:19 +0100

    add anonymous login

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/login/login.dart';
+import 'package:quiz_app/topics/topics.dart';
+import 'package:quiz_app/services/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: ElevatedButton(
-        child: Text(
-          'about',
-          style: Theme.of(context).textTheme.button,
-        ),
-        onPressed: () => Navigator.pushNamed(context, '/topics'),
-      )),
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('loading');
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text('error'),
+          );
+        } else if (snapshot.hasData) {
+          return const TopicsScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
